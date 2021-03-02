b2e6e0b06258617743f8287a47a5293590279fd5
Eric Cheung <000437017@student.vcc.ca>
2021-03-01 23:23:36 -0800

    added customer profile page

---
+++ customerProfilePage.php
@@ -0,0 +1,57 @@
+<?php 
+require_once("inc/utilities/Shared.class.php");
+Shared::header("LoGrow farm Login Page", "css/signInPage.css");
+Shared::navbar();
+?>
+
+<div class="container">
+
+<!-- Customer Info Section -->
+<div class="row container text-center mt-1 mb-2">
+    <div class="col-4">
+        <img class="img-fluid rounded mb-1" src="assets/user1.jpg" alt="profile picture">
+        <h2>Billy G.</h2>
+    </div>
+
+    <div class="card success col-8">
+    <div class="card-header"><h3>Your farm Credits:</h3></div>
+    <div class="card-body text-success">
+        <h2 class="card-title"><i class="bi bi-wallet"></i> 721</h2>
+    </div>
+    </div>
+</div>
+
+<!-- Middle Tab bar -->
+<nav>
+    <div class="row">
+    <div class="nav nav-tabs mb-1" id="nav-tab" role="tablist">
+    <a class="nav-item nav-link active col-4" id="nav-home-tab" data-toggle="tab" href="#nav-home" role="tab" aria-controls="nav-home" aria-selected="true"><i class="bi bi-clock-history"></i> My History</a>
+    <a class="nav-item nav-link col-4" id="nav-profile-tab" data-toggle="tab" href="#nav-profile" role="tab" aria-controls="nav-profile" aria-selected="false"><i class="bi bi-heart-fill"></i> My Farmers</a>
+    <a class="nav-item nav-link col-4" id="nav-contact-tab" data-toggle="tab" href="#nav-contact" role="tab" aria-controls="nav-contact" aria-selected="false"><i class="bi bi-calendar2-check"></i> My Events</a>
+  </div>
+  </div>
+</nav>
+
+<!-- Tab bar targets -->
+<div class="tab-content" id="nav-tabContent">
+
+  <div class="tab-pane fade show active" id="nav-home" role="tabpanel" aria-labelledby="nav-home-tab">
+      no shopping history
+    </div>
+
+  <div class="tab-pane fade" id="nav-profile" role="tabpanel" aria-labelledby="nav-profile-tab">
+      no favorite farmer
+    </div>
+
+  <div class="tab-pane fade" id="nav-contact" role="tabpanel" aria-labelledby="nav-contact-tab">
+      no events
+    </div>
+
+</div>
+
+<!-- End of container -->
+</div>
+
+<?php
+Shared::footer();
+?>
--- inc/utilities/Shared.class.php
+++ inc/utilities/Shared.class.php
@@ -17,6 +17,9 @@ class Shared {
 
                 <!-- Bootstrap CSS -->
                 <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-BmbxuPwQa2lc/FVzBcNJ7UAyJxM6wuqIj61tLrc4wSX0szH/Ev+nYRRuWlolflfl" crossorigin="anonymous">
+                
+                <!-- Bootstrap Icons -->
+                <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.4.0/font/bootstrap-icons.css">
 
                 <!-- CSS -->
                 <link rel="stylesheet" href="css/shared.css">
@@ -34,11 +37,47 @@ class Shared {
 
             <!-- Bootstrap Bundle with Popper -->
             <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta2/dist/js/bootstrap.bundle.min.js" integrity="sha384-b5kHyXgcpbZJO/tY9Ul7kGkf1S0CWuKcCD38l8YkeH8z8QjE0GmW1gYU5S9FOnJ0" crossorigin="anonymous"></script>
+            <script src="https://code.jquery.com/jquery-3.2.1.slim.min.js" integrity="sha384-KJ3o2DKtIkvYIK3UENzmM7KCkRr/rE9/Qpg6aAZGJwFDMVNA/GpGFF93hXpG5KkN" crossorigin="anonymous"></script>
+            <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.12.9/umd/popper.min.js" integrity="sha384-ApNbgh9B+Y1QKtv3Rn7W3mgPxhU9K/ScQsAP7hUibX39j7fakFPskvXusvfa0b4Q" crossorigin="anonymous"></script>
+            <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/js/bootstrap.min.js" integrity="sha384-JZR6Spejh4U02d8jOt6vLEHfe/JQGiRRSQQxSfFWpi1MquVdAyjUar5+76PVCmYl" crossorigin="anonymous"></script>
 
         </body>
         </html>
 
     <?php }
+
+    static function navbar() { ?>
+
+        <nav class="navbar navbar-expand-lg navbar-light bg-light">
+        <div class="container-fluid">
+
+            <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarTogglerDemo01" aria-controls="navbarTogglerDemo01" aria-expanded="false" aria-label="Toggle navigation">
+                <span class="navbar-toggler-icon"></span>
+            </button>
+
+            <a class="navbar-brand" href="#">
+                <img class="img-fluid " src="assets/logo.png" width=200 alt="LoGrow farm logo">
+            </a>
+
+            <i class="bi bi-chat-left-dots" style="font-size: 1.5rem;"></i>
+
+            <div class="collapse navbar-collapse" id="navbarTogglerDemo01">
+                <ul class="navbar-nav mr-auto mt-2 mt-lg-0">
+                <li class="nav-item">
+                    <a class="nav-link" href="#">Update my info</a>
+                </li>
+                <li class="nav-item">
+                    <a class="nav-link" href="#">Logout</a>
+                </li>
+                </ul>
+            </div>
+        </div>
+        </nav>
+
+    <?php }
+
+
+
 
 }
 
