3491be5c25635f317cca0ca6a347b42aea0378a4
Eric Cheung <000437017@student.vcc.ca>
2021-03-02 14:03:37 -0800

    added footer

--- inc/utilities/Shared.class.php
+++ inc/utilities/Shared.class.php
@@ -36,6 +36,14 @@ class Shared {
 
     static function footer() { ?>
 
+            <footer>
+            <!-- Copyright -->
+            <div class="text-center" style="background-color: rgba(0, 0, 0, 0.2);">
+            © 2021 SuperGreenTeam
+            </div>
+            <!-- Copyright -->
+            </footer>
+
             <!-- Bootstrap Bundle with Popper -->
             <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta2/dist/js/bootstrap.bundle.min.js" integrity="sha384-b5kHyXgcpbZJO/tY9Ul7kGkf1S0CWuKcCD38l8YkeH8z8QjE0GmW1gYU5S9FOnJ0" crossorigin="anonymous"></script>
             <script src="https://code.jquery.com/jquery-3.2.1.slim.min.js" integrity="sha384-KJ3o2DKtIkvYIK3UENzmM7KCkRr/rE9/Qpg6aAZGJwFDMVNA/GpGFF93hXpG5KkN" crossorigin="anonymous"></script>
