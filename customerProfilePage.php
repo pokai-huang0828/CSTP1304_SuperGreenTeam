40dfad093561e0510668af7e84ad3eeececcca6d
Po-Kai Huang <000429977@student.vcc.ca>
2021-03-12 09:28:03 -0800

    fixed pic bugs

--- customerProfilePage.php
+++ customerProfilePage.php
@@ -44,7 +44,7 @@ ProfilePage::profileCard();
     <ul class="list-group">
       <li class="list-group-item"><img src="assets/farmerIcon.png" alt="avatar1" width="40" height="40" style ="border-radius: 50%"> Kenny Huang<span style="float:right"><i class="bi bi-heart-fill"></i></span></li>
       <li class="list-group-item"><img src="assets/farmFood.jpg" alt="avatar2" width="40" height="40" style ="border-radius: 50%"> Eric C.<span style="float:right"><i class="bi bi-heart-fill"></i></span></li>
-      <li class="list-group-item"><img src="assets/noProfilePic.png" alt="avatar3" width="40" height="40" style ="border-radius: 50%"> Sehbaz Singh<span style="float:right"><i class="bi bi-heart-fill"></i></span></li>
+      <li class="list-group-item"><img src="assets/farmerIcon.png" alt="avatar3" width="40" height="40" style ="border-radius: 50%"> Sehbaz Singh<span style="float:right"><i class="bi bi-heart-fill"></i></span></li>
       <li class="list-group-item"><img src="assets/customerIcon.png" alt="avatar4" width="40" height="40" style ="border-radius: 50%"> Jimmy Link<span style="float:right"><i class="bi bi-heart-fill"></i></span></li>
     </ul>
   </div>
--- dmList.php
+++ dmList.php
@@ -15,7 +15,7 @@ Shared::dmNavbar();
         '<a href="dmDialogFarmer.php?user=farmer" style="text-decoration:none">
             <li class="list-group-item"><img src="assets/farmFood.jpg" alt="avatar2" width="40" height="40" style ="border-radius: 50%"> Billy <span class="badge badge-pill badge-primary">new</span><span style="float:right">1 min</span></li>
         </a>'?>
-        <li class="list-group-item"><img src="assets/noProfilePic.png" alt="avatar3" width="40" height="40" style ="border-radius: 50%"> Sehbaz Singh<span style="float:right">2 d</span></li>
+        <li class="list-group-item"><img src="assets/farmerIcon.png" alt="avatar3" width="40" height="40" style ="border-radius: 50%"> Sehbaz Singh<span style="float:right">2 d</span></li>
         <li class="list-group-item"><img src="assets/customerIcon.png" alt="avatar4" width="40" height="40" style ="border-radius: 50%"> Jimmy Link<span style="float:right">5 d</span></li>
     </ul>
 </div>
